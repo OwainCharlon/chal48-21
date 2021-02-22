6ea2612057513ac93a99d48556702e5d2aaf0a6f
owain <=>
2021-02-22 23:58:15 +0100

    indentation error fixed

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import pymongo
 from config import DevelopmentConfig
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'static/'
+UPLOAD_FOLDER = 'pic/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
@@ -14,21 +14,23 @@ app.config.from_object(DevelopmentConfig)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 client = pymongo.MongoClient("mongodb+srv://user1:2NvZYRipodUWsipy@cluster1.swpth.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
-db = client.chal48_passion_froid
-col = db.picture
+#db = client.nomdb
+#col = db.nomColonne
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("index.html")
+    if request.method == 'POST':
+        current_dir = os.getcwd()
+        current_path = current_dir + "\\pictures\\"
+        print( request.values.get('picture_type') )
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
 
-@app.route("/update/<path>", methods=["GET", "POST"])
-def update(path):
-    print(col.find({"path": path}))
-    return render_template("update.html", path=path)
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload_file():
@@ -43,7 +45,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect("update/"+file.filename)
+            return redirect(url_for('homepage'))
     return render_template("upload.html")
 
 
@@ -72,19 +74,17 @@ def search():
                 else:
                     search = []
 
-        if len(search):
-            response = [ a for a in col.find({
-                "$and" : search
-            }) ]
-            
-        else :
-            response = { "output": {
-                    "type" : "notify",
-                    "description" : "no entities found"
-                }
-            }
+    if len(search):
+        response = [ a for a in col.find({ "$and" : search }, {"_id" : 1}) ]
 
-        return response
+    else :
+        response = { "output": {
+                "type" : "notify",
+                "description" : "no entities found"
+            }
+        }
+            
+    return response
 
 
 if __name__ == "__main__":
