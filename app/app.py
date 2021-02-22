1b318b91dce9fdf71ace71536331ee42329a7d04
owain <=>
2021-02-22 23:25:39 +0100

    search en back is down c'est le final countdown

--- app/app.py
+++ app/app.py
@@ -48,13 +48,45 @@ def upload_file():
             return redirect(url_for('homepage'))
     return render_template("upload.html")
 
+
 @app.route("/search", methods=["GET"])
 def search():
-    #Recherche par Nom/Libelléimage -> regex du champ name
-    #Recherche par type/image -> regex du champ type image
-    #Ensemble de checkBox
-    #Nom d'auteur ->regex 
-    return
+
+    response = request.json
+    search = []
+
+    filtersDic = {
+            'name' :                '{ "name" : {"$regex": \'.*{}.*\' }}',
+            'type' :                '{ "type" : {"$regex": \'.*{}.*\' }}',
+            'credits' :             '{ "credits" : {"$regex": \'.*{}.*\' }}',
+            'with_product' :        '{ "with_product" : {"$eq" :  {} }}', 
+            'with_humans' :         '{ "with_humans" :  {"$eq" :  {} }}', 
+            'institutional' :       '{ "institutional" : {"$eq" :  {} }}',
+            'format' :              '{ "format" : {"$eq" :   {} }}', 
+            'tags' :                '{ "tags" :         {"$in" :  {} }}', 
+        }
+
+    if 'filters' in response:
+            for key, value in response['filters'].items():
+                if key in filtersDic:
+                    newFilter = filtersDic[key].replace ( '{}', str(value) )
+                    search.append( ast.literal_eval( newFilter ))
+                else:
+                    search = []
+
+        if len(search):
+            response = [ a for a in col.find({
+                "$and" : search
+            }) ]
+            
+        else :
+            response = { "output": {
+                    "type" : "notify",
+                    "description" : "no entities found"
+                }
+            }
+
+        return response
 
 
 if __name__ == "__main__":
