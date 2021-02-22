f5f7db528d1a2d950830c1df887e90fa4b4b2fb9
owain <=>
2021-02-22 11:48:23 +0100

    premier push, architecture basique

---
+++ app/app.py
@@ -0,0 +1,23 @@
+# Imports
+from flask import Flask, redirect, url_for, render_template, request, session, flash, jsonify
+import os
+import pymongo
+
+from config import DevelopmentConfig
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+
+client = pymongo.MongoClient("mongodb+srv://user1:2NvZYRipodUWsipy@cluster1.swpth.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
+#db = client.nomdb
+#col = db.nomColonne
+
+@app.route("/", methods=["GET"])
+def homepage():
+
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
